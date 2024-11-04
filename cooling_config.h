c436911d2031360bd17c394b2fa057918a2e9793
arunjoy128 <arunabrahamjoy@gmail.com>
2024-11-04 19:50:05 +0530

    Update cooling_config.h

--- cooling_config.h
+++ cooling_config.h
@@ -1,15 +1,17 @@
-#pragma once
+#include "cooling_config.h"
+#include "cooling_limits.h"
 
-typedef enum {
-    PASSIVE_COOLING,
-    HI_ACTIVE_COOLING,
-    MED_ACTIVE_COOLING
-} cooling_type;
+static const cooling_config cooling_configs[] = {
+    {PASSIVE_COOLING, PASSIVE_COOLING_LOWER_LIMIT, PASSIVE_COOLING_UPPER_LIMIT},
+    {HI_ACTIVE_COOLING, HI_ACTIVE_COOLING_LOWER_LIMIT, HI_ACTIVE_COOLING_UPPER_LIMIT},
+    {MED_ACTIVE_COOLING, MED_ACTIVE_COOLING_LOWER_LIMIT, MED_ACTIVE_COOLING_UPPER_LIMIT},
+};
 
-typedef struct {
-    cooling_type type;
-    int lower_limit;
-    int upper_limit;
-} cooling_config;
-
-cooling_config get_cooling_config(cooling_type type);
+cooling_config get_cooling_config(cooling_type type) {
+    for (int i = 0; i < sizeof(cooling_configs) / sizeof(cooling_configs[0]); ++i) {
+        if (cooling_configs[i].type == type) {
+            return cooling_configs[i];
+        }
+    }
+      return cooling_configs[0];  // Default to PASSIVE_COOLING
+}
